c988cb82951087c0e87f4354e44a399bdb1253e4
Lukas <schoenlukas98@outlook.de>
2024-07-27 19:21:46 +0200

    incorrect labeling fixed

--- src/main/java/com/schoenl/springboottestrange/note/NoteController.java
+++ src/main/java/com/schoenl/springboottestrange/note/NoteController.java
@@ -1,6 +1,5 @@
 package com.schoenl.springboottestrange.note;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/pots")
+@RequestMapping("/notes")
 public class NoteController {
 
     private final NoteService noteService;
